349937fdfc13f61c498ed99dd41c0274f7baa4e2
baybeboy12 <quangdungzkh@gmail.com>
2023-09-29 02:55:12 +0700

    week03

---
+++ Week_03/Shopee/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import * as React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import FirstScreen from "./week3_screen/FirstScreen";
+import Bai1_a from "./week3_screen/Bai1_a";
+import Bai1_b from "./week3_screen/Bai1_b";
+import Bai1_c from "./week3_screen/Bai1_c";
+export default function App() {
+  return (
+    // <FirstScreen />
+    // <Bai1_a />
+    //<Bai1_b />
+    <Bai1_c />
+  );
+}
