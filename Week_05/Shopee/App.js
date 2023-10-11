255f01e8b53773e8aeadb923cac23196f3528067
baybeboy12 <quangdungzkh@gmail.com>
2023-10-11 17:53:48 +0700

    Week_05

---
+++ Week_05/Shopee/App.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Screen1 from "./manhinh/Screen1";
+import Screen2 from "./manhinh/Screen2";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
